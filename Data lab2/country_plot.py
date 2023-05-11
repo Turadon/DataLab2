b7e2c48b71f64351b9a828bdc32b1f8b593db3a0
Alexander Ortlieb <alexander.ortlieb94@gmail.com>
2023-05-11 15:47:23 +0200

    changes for better plotting

--- Data lab2/country_plot.py
+++ Data lab2/country_plot.py
@@ -1,31 +1,51 @@
 #%%
-import matplotlib
 import matplotlib.pyplot as plt
 import geopandas as gpd
 import pandas as pd
+import shapely as shp
 
 #%%
 
 
 #%%
+# load the Ukraine data, from this we only need the geometry (multipolygon) to plot
 ua = gpd.read_file(r'.\Ukraine\UKR_adm0.shp')
+ua_boarder = ua['geometry'][0]
 
 # %%
-firms = gpd.read_file(r'.\MODIS_C6_1_Europe_24h\MODIS_C6_1_Europe_24h.shp')
+# load satellite data
+modis = gpd.read_file(r'.\MODIS_C6_1_Europe_24h\MODIS_C6_1_Europe_24h.shp')
+viirs = gpd.read_file(r'.\J1_VIIRS_C2_Europe_24h\J1_VIIRS_C2_Europe_24h.shp')
+suomi_viirs = gpd.read_file(r'.\SUOMI_VIIRS_C2_Europe_24h\SUOMI_VIIRS_C2_Europe_24h.shp')
+
+#%%
+# return points in given counrty as list
+# input, gpd.dataframe with points and gpd.dataframe of country
+def points_in_country(pointsdf,countrydf):
+    points = pointsdf['geometry']
+    country_boarder = countrydf['geometry'][0]
+    mask = [country_boarder.contains(p) for p in list(points)]
+    return points[mask]
+
+#%%
+# then we can select only the points that are containt in Ukraine from modis
+modis_in_ua = points_in_country(modis,ua)
 
 # %%
-# use for selecting points in certain country, doesn't work yet
-from geopandas.tools import sjoin
-firms_in_ua = sjoin(firms, ua, how = 'left')
-firms_in_ua.head()
+# plot points from satellite in Ukraine
+def plot_points_in_country(pointsdf,countrydf):
+    points = points_in_country(pointsdf,countrydf)
+    fig,ax = plt.subplots(figsize = (15,15))
+    countrydf.plot(ax = ax)
+    points.plot(ax = ax, color = 'r')
+
+#%%
+# plots the modis data in Ukraine
+plot_points_in_country(modis,ua)
 
 # %%
-fig,ax = plt.subplots(figsize = (15,15))
-ua.plot(ax = ax)
-firms_in_ua.plot(ax = ax, color = 'r')
+plot_points_in_country(viirs,ua)
 
-# 
-minx, miny, maxx, maxy = ua.total_bounds
-ax.set_xlim(minx, maxx)
-ax.set_ylim(miny, maxy)
+# %%
+plot_points_in_country(suomi_viirs,ua)
 # %%
